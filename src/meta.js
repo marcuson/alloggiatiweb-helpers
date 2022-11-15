b2ffbe399c1ad36b7e6d2f310cd54ad59255cd1f
Leonardo Marcucci <marcuson.nn90@gmail.com>
2022-11-15 14:49:55 +0100

    Add skeleton.

---
+++ src/meta.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name        Helper for AlloggiatiWeb
+// @namespace   marcuson
+// @description Helpers for Alloggiati Web.
+// @match       https://alloggiatiweb.poliziadistato.it/AlloggiatiWeb/SecurityCODS.aspx
+// @grant       GM_addStyle
+// @grant       GM.getValue
+// @grant       GM.setValue
+// @version     process.env.VERSION
+// @author      process.env.AUTHOR
+// @require     https://cdn.jsdelivr.net/combine/npm/@violentmonkey/dom@2,npm/@violentmonkey/ui@0.7
+// ==/UserScript==
+
+/**
+ * - The `@grant`s used in your source code will be added automatically by `rollup-plugin-userscript`.
+ *   However you have to add explicitly those used in required resources.
+ * - `process.env.VERSION` and `process.env.AUTHOR` will be loaded from `package.json`.
+ */
